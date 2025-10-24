7666a7576e9dd17c403beeeebdf96f59807fa4b5
Thando <138705769+ThandoSA@users.noreply.github.com>
2025-10-24 17:10:44 +0200

    commit changes to main

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express');
+import express from "express";
+import connectDB from "./db.js";
+import cors from "cors";
+
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
-// Route
-app.get('/', (req, res) => {
-  res.send('Welcome to ByteBucks API 🚀');
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to ByteBucks API 🚀");
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
